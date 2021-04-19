ad7bfd7f6992701e7c7d653a98ce9594bd025751
Stepan <stepan1q1@gmail.com>
2021-04-19 21:47:54 +0300

    1 commit main

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -19,6 +19,16 @@
  *
  */
 module.exports = {
+    myTheme: {
+        area: 'frontend',
+        name: 'myVendor/myTheme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l',
+         ],
+         dsl: 'less'
+    },
     blank: {
         area: 'frontend',
         name: 'Magento/blank',
