820c77795c221d2d4782484472c4285da2f63584
Stepan <stepan1q1@gmail.com>
2021-04-21 10:19:50 +0300

    form editing complete

--- app/design/frontend/myVendor/myTheme/Magento_Catalog/web/js/my-form-modal.js
+++ app/design/frontend/myVendor/myTheme/Magento_Catalog/web/js/my-form-modal.js
@@ -1,25 +1,46 @@
 define([
     'jquery',
     'Magento_Ui/js/modal/modal',
-    'mage/translate',
-    'Magento_Customer/js/customer-data'
-    
-], function($, modal) {
-    var options = {
-        type: 'popup',
-        responsive: true,
-        innerScroll: true,
-        title: $.mage.__('My form'),
-        buttons: [{
-            text: $.mage.__('Continue'),
-            class: '',
-            click: function () {
-                this.closeModal();
-            }
-        }]
-    };
-    var popup = modal(options, $('.pop-up'));
-    $('.my-request-bottom').click(function(){
-      $('.pop-up').modal('openModal');
-    });
+    'Magento_Customer/js/customer-data',
+    'mage/translate'
+], function($, modal, customerData) {
+
+    $.widget ('custom.mymodal', {
+
+        options: {
+            type: 'popup',
+            responsive: true,
+            innerScroll: true,
+            title: $.mage.__('My form'),
+            buttons: [{
+                text: $.mage.__('Continue'),
+                class: '',
+                click: function () {
+                    this.closeModal();
+                }
+            }]
+        },
+
+        _create: function () {
+            this.initModal();
+            this.populateData();
+         },
+
+         initModal: function() {
+            var popup = modal(this.options, $('.pop-up'));
+            $('.my-request-bottom').click(function(){
+                $('.pop-up').modal('openModal');
+            });
+         },
+
+         populateData: function() {         
+            let customer = customerData.get('customer')();
+            let firstMame = customer.fullname.split(" ")[0];
+            let lastName = customer.fullname.split(" ")[1];
+            $('#fname').val(firstMame);
+            $('#lname').val(lastName);
+         }
+     });
+     
+    return $.custom.mymodal;
 });
--- app/design/frontend/myVendor/myTheme/requirejs-config.js
+++ app/design/frontend/myVendor/myTheme/requirejs-config.js
@@ -1,7 +1,6 @@
 const config = {
     map: {
         '*': {
-            my_slider: 'js/my-slider' ,
             slick: 'js/slick.min',
             my_form_modal: 'Magento_Catalog/js/my-form-modal',
         },
