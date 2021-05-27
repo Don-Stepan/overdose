13929e875bdf96722b518b01ecc6d7380a54419e
Stepan <stepan1q1@gmail.com>
2021-05-27 11:01:38 +0300

    changed the mobile button

--- app/design/frontend/myVendor/myTheme/Magento_Catalog/web/js/mobile-button-popup.js
+++ app/design/frontend/myVendor/myTheme/Magento_Catalog/web/js/mobile-button-popup.js
@@ -4,42 +4,46 @@ define([
 ], function($, mediaCheck) {
 
     $.widget ('custom.categoryButton', {
+        sidebarSelector: '.sidebar-main',
+
         _create: function () {
             mediaCheck({
                 media: '(min-width: 768px)',
                 // Switch to Desktop Version
                 entry: function () {
-                    $( ".sidebar-main" ).removeClass( "mobile-v-category");
-                    $('.sidebar-main').css({"display":"block"});
+                    $(self.sidebarSelector).removeClass( "mobile-v-category");
+                    $(self.sidebarSelector).css({"display":"block"});
                 },
                 // Switch to Mobile Version
                 exit: function () {
-                    $( ".sidebar-main" ).addClass( "mobile-v-category");
-                    $('.sidebar-main').css({"display":"none"});
+                    $(self.sidebarSelector).addClass( "mobile-v-category");
+                    $(self.sidebarSelector).css({"display":"none"});
 
                     $('.mobile-category-button').click(function (){
-                        $('.sidebar-main').show();
-                        $('.page-header').hide();
-                        $("body").css({"overflow":"hidden"});
-                        $('.sidebar-main').css({"padding-right":"0"});
+                        self.openPopup();
                     });
                     $('.filter-header-close').click(function (){
-                        $('.sidebar-main').hide();
-                        $('.page-header').show();
-                        $("body").css({"overflow":"scroll"});
+                        self.closePopup();
                     });
                     $('.category-button-popup').click(function () {
-                        $('.sidebar-main').hide();
-                        $('.page-header').show();
-                        $("body").css({"overflow":"scroll"});
+                        self.closePopup();
                     });
+                },
+                openPopup: function() {
+                    $(self.sidebarSelector).show();
+                    $('.page-header').hide();
+                    $("body").css({"overflow":"hidden"});
+                    $(self.sidebarSelector).css({"padding-right":"0"});
+                },
+                closePopup: function() {
+                    $(self.sidebarSelector).hide();
+                    $('.page-header').show();
+                    $("body").css({"overflow":"scroll"});
                 }
             });
         },
     });
-
     return $.custom.categoryButton;
-
 });
 
 
